aa43187383e88117ef22f93887d529e6e341990b
Kavin Adithiya <kavin.avs@gmail.com>
2021-01-24 03:16:08 -0500

    Adds start of multiple clients and fixes some issues

--- proxy.py
+++ proxy.py
@@ -1,21 +1,30 @@
 import sys, os, time, socket, select
 
+
 def get_header_val(name, header):
     index = header.find(name)
+    if index == -1:
+        return -1
     index += len(name)+1
     num = header[index:].split(b"\r\n", 1)
     return num[0].strip()
 
+
 def receive_webinfo(webaddress, request):
     webpage = b""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (webaddress, 80)
-        sock.connect(server_address)
+        address = (webaddress, 80)
+        sock.connect(address)
         sock.sendall(request)
 
         data = sock.recv(1024)
-        content_len = int(get_header_val(b"Content-Length", data))
+        num = get_header_val(b"Content-Length", data)
+        if num == -1:
+            sock.close()
+            return data
+
+        content_len = int(num)
 
         content_index = data.find(b"\r\n\r\n") + 4
         curr_len = len(data[content_index:])
@@ -25,55 +34,73 @@ def receive_webinfo(webaddress, request):
             webpage += data
             curr_len += len(data)
 
-    except socket.error:
+    except Exception:
         print("Unable to connect to web server")
         sock.close()
-
 
     else:
         sock.close()
 
     return webpage
 
+
+def handle_request(sock):
+    request = b""
+    while True:
+        data = sock.recv(1024)
+        # print('{!r}'.format(data))
+        if data:
+            request += data
+            if request[-4::] == b"\r\n\r\n":
+                break
+        else:
+            # print('no data from', client_address)
+            break
+
+    if request == b'':
+        return
+
+    start_index = request.find(b"Host:")
+    if start_index != -1:
+        request = request[:start_index] + request[start_index:].split(b"\r\n", 1)[1]
+
+    # get web address and add http:// to the header's address
+    temp = request.split(b" ", 2)
+    web_address = temp[1].split(b"/")[1]  #get address ([0] is empty since it starts with /)
+    request = temp[0] + b" http://" + temp[1].strip(b"/") + b" " + temp[2]
+
+    webpage = receive_webinfo(web_address, request)
+    sock.sendall(webpage)
+
+
 if __name__ == "__main__":
     # creates socket
+    inputs = []
     try:
         server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.setblocking(False)
         server_address = ('localhost', 8888)
         server_sock.bind(server_address)
-        server_sock.listen(1)
-    except Exception:
-        sys.exit(1)
+        server_sock.listen(5)
 
-    while True:
-        # wait for connection
-        connection, client_address = server_sock.accept()
+        inputs.append(server_sock)
+        outputs = []
 
-        try:
-            # Receive the data in small chunks and retransmit it
-            request = b""
-            while True:
-                data = connection.recv(1024)
-                #print('{!r}'.format(data))
-                if data:
-                    request += data
-                    if request[-4::] == b"\r\n\r\n":
-                        break
+        while inputs:
+            readable, writable, exceptional = select.select(inputs, outputs,
+                                                            inputs)
+
+            for s in readable:
+                if s is server_sock:
+                    connection, client_address = s.accept()
+                    connection.setblocking(0)
+                    inputs.append(connection)
                 else:
-                    #print('no data from', client_address)
-                    break
+                    handle_request(s)
 
-            start_index = request.find(b"Host:")
-            request = request[:start_index] + request[start_index:].split(b"\r\n", 1)[1]
+    except socket.error as e:
+        print(e)
 
-            temp = request.split(b" ", 2)
-            web_address = temp[1].strip(b"/")
-            request = temp[0] + b" http://" + web_address + b" " + temp[2]
-
-            webpage = receive_webinfo(web_address, request)
-            connection.sendall(webpage)
-
-
-        finally:
-            # close connecton
-            connection.close()
+    for s in inputs:
+        s.close()
+    sys.exit(1)
