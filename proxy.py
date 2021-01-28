bc1df3024912faa97b774dc770e3a3402ce20ef9
Kavin Adithiya <kavin.avs@gmail.com>
2021-01-27 23:04:59 -0500

    Reverted back to before persistant changes

--- proxy.py
+++ proxy.py
@@ -24,30 +24,19 @@ def get_header_val(name, header):
     return num[0].strip()
 
 
-def contact_webserver(webaddress, request, client_socket):
+def contact_webserver(webaddress, request):
     """
     Creates a client to request data from given web address using the
     request header
-    :param client_socket: socket of the client connection
-                           (used index into socket_pair dictionary)
     :param webaddress: The address of the web server
     :param request: The request header to send to the web server
     :return: the webpage/file sent by the web server
     """
     webpage = b""
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        # check if client already has a connection
-        if client_socket in socket_pairs.keys():
-            # print("used old socket for web server")
-            sock = socket_pairs[client_socket]
-        else:
-            # print("made new socket to connect to webserver")
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            address = (webaddress, 80)
-            sock.connect(address)
-            socket_pairs[client_socket] = sock
-
+        address = (webaddress, 80)
+        sock.connect(address)
         sock.sendall(request)
 
         data = sock.recv(1024)
@@ -67,6 +56,7 @@ def contact_webserver(webaddress, request, client_socket):
         else:  # handle other transfer encoding that have content length on them
             num = get_header_val(b"Content-Length", data)
             if num == -1:
+                sock.close()
                 return data
 
             content_len = int(num)
@@ -81,43 +71,14 @@ def contact_webserver(webaddress, request, client_socket):
     except socket.error:
         print("Unable to connect to web server")
         sock.close()
-        socket_pairs.pop(client_socket)
+
+    else:
+        sock.close()
 
     return webpage
 
 
-def modify_header(request):
-
-    # get web address
-    temp = request.split(b" ", 2)
-    address_parts = temp[1].split(b"/", 2)
-    host = address_parts[1]  # since address starts with / [1] is the domain
-
-    # change GET path
-    path = b""
-    if len(address_parts) < 3:
-        path = b" / "
-        # if the address is "/www.example.com" then this only has 2 parts
-    else:
-        path = b" /" + address_parts[2] + b" "
-    request = temp[0] + path + temp[2]
-
-    # change HOST header with domain address
-    start_index = request.find(b"Host:")
-    if start_index != -1:
-        request = request[:start_index] + b"Host: " + host + b" \r\n"\
-                  + request[start_index:].split(b"\r\n", 1)[1]
-
-    # change the Accept-Encoding header value so the web server returns ascii
-    start_index = request.find(b"Accept-Encoding:")
-    if start_index != -1:
-        request = request[:start_index] + b"Accept-Encoding: " + b"identity\r\n"\
-                  + request[start_index:].split(b"\r\n", 1)[1]
-
-    return request, host, path
-
-
-def retrieve_webpage(host, path, request, socket):
+def retrieve_webpage(host, path, request):
     """
     Grabs web page from cache or retrieves from web server if not cached
 
@@ -145,10 +106,10 @@ def retrieve_webpage(host, path, request, socket):
 
         # removes expired caches
         if curr_age > allowed_age:
-            # print(b"Updating old cache files for " + host + path.strip())
+            print("Updating cache files")
             os.remove(filepath)
 
-            webpage = contact_webserver(host, request, socket)
+            webpage = contact_webserver(host, request)
             # saves webpage data in a file
             f = open(filepath, 'wb')
             f.write(webpage)
@@ -156,12 +117,12 @@ def retrieve_webpage(host, path, request, socket):
         else:
             f = open(filepath, 'rb')
             webpage = f.read()
-            # print(b"got " + host + path.strip() + b" from cache")
+            print("got from cache")
 
     # get data if not cached
     else:
         # gets data from host server
-        webpage = contact_webserver(host, request, socket)
+        webpage = contact_webserver(host, request)
 
         # saves webpage data in a file
         f = open(filepath, 'wb')
@@ -180,7 +141,7 @@ def handle_request(sock):
     request = b""
     while True:
         data = sock.recv(1024)
-        # print('{!r}'.format(data))
+        print('{!r}'.format(data))
         if data:
             request += data
             if request[-4::] == b"\r\n\r\n":
@@ -192,18 +153,41 @@ def handle_request(sock):
     if request == b'':
         return
 
-    request, host, path = modify_header(request)
+    # get web address
+    temp = request.split(b" ", 2)
+    address_parts = temp[1].split(b"/", 2)
+    host = address_parts[1]  # since address starts with / [1] is the domain
+
+    # change GET path
+    path = b""
+    if len(address_parts) < 3:
+        path = b" / "
+        # if the address is "/www.example.com" then this only has 2 parts
+    else:
+        path = b" /" + address_parts[2] + b" "
+    request = temp[0] + path + temp[2]
+
+    # change HOST header with domain address
+    start_index = request.find(b"Host:")
+    if start_index != -1:
+        request = request[:start_index] + b"Host: " + host + b" \r\n"\
+                  + request[start_index:].split(b"\r\n", 1)[1]
+
+    # change the Accept-Encoding header value so the web server returns ascii
+    start_index = request.find(b"Accept-Encoding:")
+    if start_index != -1:
+        request = request[:start_index] + b"Accept-Encoding: " + b"identity\r\n"\
+                  + request[start_index:].split(b"\r\n", 1)[1]
 
     # get webpage and send it to client
-    webpage = retrieve_webpage(host, path, request, socket)
+    webpage = retrieve_webpage(host, path, request)
+    print("WEBPAGE:")
+    print(webpage)
     sock.sendall(webpage)
 
 
-socket_pairs = {}
-
 if __name__ == "__main__":
     # creates socket
-    request_time = {}
     inputs = []
     try:
         server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -224,26 +208,8 @@ if __name__ == "__main__":
                     connection, client_address = s.accept()
                     connection.setblocking(0)
                     inputs.append(connection)
-                    request_time[connection] = time.time()
                 else:
-
                     handle_request(s)
-
-            socket_list = []
-            for s in request_time.keys():
-                # check all clients for inactive or errored out sockets
-                if ((time.time() - request_time[s]) > 10) or (s in exceptional):
-                    # can't pop from dictionary during loop
-                    socket_list.append(s)
-
-            for s in socket_list:
-                # close connection and remove them form lists
-                if s in socket_pairs.keys():
-                    socket_pairs[s].close()
-                    socket_pairs.pop(s, None)
-                s.close()
-                request_time.pop(s, None)
-                inputs.remove(s)
 
     except socket.error as e:
         print(e)
